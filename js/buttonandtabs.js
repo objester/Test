aad3604fe2e39263e9c6897208640a9d05329653
objester <objester@gmail.com>
2017-08-20 18:35:17 -0500

    no message

---
+++ js/buttonandtabs.js
@@ -0,0 +1,17 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope) {
+
+
+  // $scope.tabs = [
+  //   { title:'Stores', content:'List of stores' },
+  //   { title:'Products', content:'List of products' },
+  //   { title:'Shopping List', content:'List of all products' },
+  //   { title:'Go Shopping', content:'Things I need this week' }
+  // ];
+
+  // $scope.model = {
+  //   name: 'Tabs'
+  // };
+
+
+});
